1c99939dbec7641b0f169c36473adedcd419c722
shizi <wj06_job@163.com>
2017-12-04 18:39:52 +0800

    deploy

---
+++ src/main/java/com/demo/config/server/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.demo.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author wwchang
+ * @date 2017/12/4 18:21
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
